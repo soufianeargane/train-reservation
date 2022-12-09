8b2d3a9259a3d27a480f70560675bf83f48bd1c1
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2022-12-09 10:24:39 +0100

    the push

--- signupconfig.php
+++ signupconfig.php
@@ -9,49 +9,47 @@ class signupconfig
       private $password;
       protected $dbcon;
 
-      public function __construct($name ="",$email="",$password="")
+      public function __construct($name = "", $email = "", $password = "")
       {
-            $this ->name =$name;
-            $this ->email = $email;
-            $this ->password = $password;
+            $this->name = $name;
+            $this->email = $email;
+            $this->password = $password;
             // $this ->dbcon = new PDO(DB_TYPE.":host =".DB_HOST.";dbname =".DB_NAME, DB_USER,DB_PWD);
-            $this ->dbcon = new PDO("mysql:host=localhost;dbname=sgtt", 'root', '');
+            $this->dbcon = new PDO("mysql:host=localhost;dbname=sgtt", 'root', '');
       }
       public function setname($name)
       {
-            $this ->name = $name;
+            $this->name = $name;
       }
       public function getname()
       {
-            return $this -> name;
+            return $this->name;
       }
       public function setemail($email)
       {
-            $this ->email = $email;
+            $this->email = $email;
       }
       public function getemail()
       {
-            return $this -> email;
+            return $this->email;
       }
       public function setpass($password)
       {
-            $this ->password = $password;
-      }   
+            $this->password = $password;
+      }
       public function getpass()
       {
-            return $this -> password;
+            return $this->password;
       }
       public function insertData()
       {
-            try{
-                  $data = ['name' => $this->name,'email' => $this->email, 'password' => $this->password, 'role'=>0];
+            try {
+                  $data = ['name' => $this->name, 'email' => $this->email, 'password' => $this->password, 'role' => 0];
                   $sql  = "INSERT INTO user(name, email, password, role) VALUES (:name,:email,:password,:role)";
-                  $stmt = $this -> dbcon-> prepare($sql);
-                  $stmt -> execute($data);
-               }catch(Exeption $e){return $e -> getMessage();}
+                  $stmt = $this->dbcon->prepare($sql);
+                  $stmt->execute($data);
+            } catch (Exception $e) {
+                  return $e->getMessage();
+            }
       }
-
-
 }
-
-?>
